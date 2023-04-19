049a0e99529b53feb8c0710007b1ceb09c7c7d94
okekevicktur <okekevicktur@gmail.com>
2023-04-19 15:26:07 +0100

    fixed cursor pointer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,10 +6,10 @@ const Nav = () => {
       <img src={logo} alt="instagram-text-logo" className="logo" />
       <input type="search" placeholder="Search" className="searchInput"></input>
       <div className="iconWrapper">
-        <i className="fa-solid fa-house-chimney"></i>
-        <i class="fa-regular fa-message"></i>
-        <i class="fa-regular fa-compass"></i>
-        <i class="fa-regular fa-heart"></i>
+        <i className="navlinks fa-solid fa-house-chimney"></i>
+        <i class=" navlinks fa-regular fa-message"></i>
+        <i class=" navlinks fa-regular fa-compass"></i>
+        <i class=" navlinks fa-regular fa-heart"></i>
       </div>
     </div>
   );
